a22d82605c0da188b343d71225c0336b04ea42ce
Rahul bhati <rahulbhati1127@gmail.com>
2026-02-04 14:40:11 +0530

    update database url in env.py , config

--- migrations/env.py
+++ migrations/env.py
@@ -34,6 +34,10 @@ if not DATABASE_URL:
 # Alembic Config object
 config = context.config
 
+
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
+
+
 # Logging
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
