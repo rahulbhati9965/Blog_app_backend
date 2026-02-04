cd0e2d2c5202777b0c07aa6323bf800363487a1a
rahulbhati9965 <rahulbhati1127@gmail.com>
2026-02-04 13:21:21 +0530

    Make DB wait script Render-compatible

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,9 +1,28 @@
+# #!/bin/sh
+# set -e
+
+# echo "Waiting for postgres..."
+
+# until psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -c "SELECT 1;" > /dev/null 2>&1; do
+#   sleep 1
+# done
+
+# echo "Postgres is up!"
+
+# echo "Running migrations..."
+# alembic upgrade head
+
+# echo "Starting uvicorn..."
+# exec "$@"
+
+
+
 #!/bin/sh
 set -e
 
 echo "Waiting for postgres..."
 
-until psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -c "SELECT 1;" > /dev/null 2>&1; do
+until pg_isready -d "$DATABASE_URL" > /dev/null 2>&1; do
   sleep 1
 done
 
